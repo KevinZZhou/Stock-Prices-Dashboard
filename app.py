890e5294fd735f551c0817c6b5097ccac79e6bd7
Kevin Zhou <53985119+KevinZZhou@users.noreply.github.com>
2021-01-06 18:58:29 -0500

    Update app.py
    Used Bootstrap components in dashboard
    Added user input functionality
    
    TODO:
    Handle user input exceptions
    Stylesheet?

--- app.py
+++ app.py
@@ -4,27 +4,68 @@ import plotly.express as px
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import dash_bootstrap_components as dbc
 from dash.dependencies import Input, Output
 
 # Initialize the Dash app
-app = dash.Dash(__name__)
+app = dash.Dash(__name__, external_stylesheets = [dbc.themes.SOLAR])
 
 # Setup the Dash app layout
 app.layout = html.Div(
-    children = [html.Div(
-        className = "row", 
-        children = [
+    className = "", 
+    children = [dbc.Row(children = [
+        dbc.Col(
             html.Div(
-                className = "three columns div-user-controls", 
-                children = []
+                className = "", 
+                children = [
+                    html.H2("Stock Market Dashboard"),
+                    html.H6("Created with Plotly Dash"),
+                    dbc.Input(
+                        id = "stock-ticker-input", 
+                        value = "", 
+                        placeholder = "Enter a valid stock ticker...", 
+                        type = "text"
+                    )
+                ]
             ), 
+            width = 4
+        ), 
+        dbc.Col(
             html.Div(
-                className = "nine columns div-for-charts bg-grey", 
-                children = []
-            )
-        ]
-    )]
+                className = "", 
+                children = [
+                    html.Div(id = "closing-prices")
+                ]
+            ), 
+            width = 8
+        )
+    ])]
 )
+
+# Callback to get the corresponding closing prices graph based on user input
+@app.callback(Output("closing-prices", "children"), 
+             [Input("stock-ticker-input", "value")])
+def update_closing_prices_graph(input): 
+    start = datetime.datetime(2010, 1, 1)
+    end = datetime.datetime.now()
+
+    stock_df = web.DataReader(input, "yahoo", start, end)
+
+    graph = dcc.Graph(
+        id = "closing-prices-graph", 
+        figure = {
+            "data": [{
+                "x": stock_df.index, 
+                "y": stock_df.Close, 
+                "type": "line"
+            }], 
+            "layout": {
+                "title": input
+            }
+        }, 
+        animate = True
+    )
+    return graph
 
 # Run the Dash app
 if __name__ == '__main__':
